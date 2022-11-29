dc29c8fe1bc9f180db024e7d5606f69ffe26f783
Irving Ramírez García <irving.ramirez.garcia@ericsson.com>
2022-11-29 12:42:46 +0100

    irvingmx-0005 - Adding properties for ScoreChecker and MailService

--- src/main/java/irvingmx/bank/service/EmailServiceImpl.java
+++ src/main/java/irvingmx/bank/service/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package irvingmx.bank.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,8 +10,8 @@ import org.springframework.stereotype.Service;
 @Service("EmailService")
 public class EmailServiceImpl implements EmailService {
 
-    private static final String NOREPLY_ADDRESS = "iramirezg@gmail.com";
-
+    @Value("${irvingmx.emailService.noReplyAddress}")
+    private String NOREPLY_ADDRESS;
     @Autowired
     private JavaMailSender emailSender;
 
