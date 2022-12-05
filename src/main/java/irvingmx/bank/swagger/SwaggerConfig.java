a4744d69e04c714e75e986048534746286024df0
Irving Ramírez García <irving.ramirez.garcia@ericsson.com>
2022-12-05 10:16:31 +0100

    irvingmx-0010 - Adding an API

--- src/main/java/irvingmx/bank/swagger/SwaggerConfig.java
+++ src/main/java/irvingmx/bank/swagger/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
                 .securityContexts(Arrays.asList(securityContext()))
                 .securitySchemes(Arrays.asList(apiKey()))
             .select()
-            .apis(RequestHandlerSelectors.basePackage("irvingmx.bank.api.v1"))
+            .apis(RequestHandlerSelectors.basePackage("irvingmx.bank.resources"))
             .paths(PathSelectors.any())
             .build();
     }
